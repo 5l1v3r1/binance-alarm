26116f364313597ea3278c310dd855f0a662f676
arabacibahadir <bahadirarabaci@yandex.com>
2022-08-19 16:48:30 +0300

    WIP windows alert

--- alarm_alert.py
+++ alarm_alert.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from binance.client import Client
+from winotify import Notification, audio
 
 alarm_list = 'alarm1', 'alarm2', 'alarm3', 'alarm4', 'alarm5', \
              'alarm6', 'alarm7', 'alarm8', 'alarm9', 'alarm10', \
@@ -8,31 +10,36 @@ alarm_list = 'alarm1', 'alarm2', 'alarm3', 'alarm4', 'alarm5', \
 
 
 def alert_dict():
+    client = Client("", "")
     all_data = {}
     csv_alarm = pd.read_csv('alarm_data.csv')
+    df = pd.DataFrame(client.get_all_tickers(), columns={'symbol': str(), 'price': float()})
+    df.to_csv('api_data.csv', index=False)
     csv_api_prices = pd.read_csv('api_data.csv')
-    # print(db_pandas.get_alarms())
-    # find all the coins that have an alarm count greater than 0
     index_list = csv_alarm['alarm_count'].loc[csv_alarm['alarm_count'] > 0].index
-    # loop through the index list and print the symbol and alarm count for each coin
     for index in index_list:
         symbol = csv_alarm.at[index, 'symbol']
         alarm_count = csv_alarm.at[index, 'alarm_count']
-        # print(f'{symbol}: {alarm_count}')
-        # find all alarms
         alarms = []
         for i in alarm_list:
             if csv_alarm.at[index, i] != 0:
                 alarms.append(csv_alarm.at[index, i])
-        # find the price of the coin
         price = csv_api_prices.loc[csv_api_prices['symbol'] == symbol, 'price'].values[0]
         closest_alarm = min(alarms, key=lambda x: abs(x - price))
         percentage_difference = abs(price - closest_alarm) / price * 100
-        # create dictionary to store the data
         all_data[symbol] = {'alarm_count': alarm_count, 'alarms': alarms, 'price': price,
                             'closest_alarm': closest_alarm, 'percentage_difference': percentage_difference}
-        # print(f'{symbol}: %{percentage_difference=} {price=} : {closest_alarm=}')
-    return all_data
+
+        if percentage_difference < 10:
+            alert_msg = f"{symbol} : {price} \n" \
+                        f"Difference : {percentage_difference:.2f}%\n" \
+                        f"Alarm : {closest_alarm}"
+            toast = Notification(app_id="SupRes", title="Alert", msg=alert_msg, duration="short")
+            toast.add_actions(label=f"Go to Spot {symbol}", launch=f"https://www.binance.com/en/trade/{symbol}")
+            toast.add_actions(label=f"Go to Futures {symbol}", launch=f"https://www.binance.com/en/futures/{symbol}")
+            toast.set_audio(audio.Mail, loop=False)
+            toast.show()
+    # return all_data
 
 
 print(alert_dict())
